a2546d703d9af394a041d624a529a3f2967555f0
mteste023 <alleffsilvaleitee@gmail.com>
2025-06-14 12:47:49 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api")
+def api():
+    url = "https://bac-bo-brazilian-api.p.rapidapi.com/res_bacbo"
+    headers = {
+        "x-rapidapi-key: 0c07257e8dmsha69b56748a8a1afp1a39f9jsnf8b438cf1101",  # Substitua pela sua chave real do RapidAPI
+        "x-rapidapi-host: bac-bo-brazilian-api.p.rapidapi.com"
+    }
+
+    try:
+        res = requests.get(url, headers=headers, timeout=10)
+        res.raise_for_status()
+        data = res.json()
+
+        historico = [{
+            "player": h["playerTotal"],
+            "banker": h["bankerTotal"],
+            "winner": h["winner"]
+        } for h in data.get("res_bacbo", [])[:20]]
+
+        return jsonify({"historico": historico})
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
