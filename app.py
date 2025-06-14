206b96ee4658815f7b4261530a6075f0d1626260
mteste023 <alleffsilvaleitee@gmail.com>
2025-06-14 12:08:44 -0300

    app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ CORS(app)
 
 @app.route("/api")
 def api():
-    url = "https://binhole-bac-bo-brazilian-api.p.rapidapi.com/history"
+    url = "https://bac-bo-brazilian-api.p.rapidapi.com/res_bacbo"
     headers = {
         "X-RapidAPI-Key": "0c07257e8dmsha69b56748a8a1afp1a39f9jsnf8b438cf1101",  # Substitua pela sua chave real do RapidAPI
-        "X-RapidAPI-Host": "binhole-bac-bo-brazilian-api.p.rapidapi.com"
+        "X-RapidAPI-Host": "bac-bo-brazilian-api.p.rapidapi.com"
     }
 
     try:
